5bc6ac302bd36b9b46da792e1ca35badf47da743
Jenna Ritten <jritten@umich.edu>
2017-04-30 13:03:29 -0400

    challeng 5.6: problem solving methods progress

---
+++ ruby/problem_solving_methods.rb
@@ -0,0 +1,66 @@
+
+# Release 0: Implement a Simple Search
+
+# create a counter starting at 0
+# for each value of array
+  # check if item equals target
+  # if so, return count
+  # else, keep going and add one to  count
+# no items match, return nil
+
+def search_array(arr, num)
+	index = 0
+
+	arr.each do |number|
+		if number == num
+			p index
+		else
+			index += 1
+		end
+	end
+	p nil
+end
+
+search_array([72, 95, 26, 1], 4)
+
+def search_array(arr, num)
+	index = 0
+
+	arr.length.times do |index|
+		if arr[index] == num
+			puts index
+		else
+			index += 1
+		end
+	end
+	puts nil
+end
+
+search_array([72, 95, 26, 4], 4)
+
+# Release 1: Calculate Fibonacci Numbers
+
+# start off with 0 and 1
+# the next number is equal to the sum of the last two
+# except for 0 and 1 we started with
+# print results in an array
+
+def fib(bit)
+	fib_array = [0]
+	x = 0
+	y = 1
+	z = 0
+	if bit >= 2
+		fib_array = [*1..bit]
+			until fib_array.count
+				z = x + y
+				y = x
+				z = y 
+				fib_array << j
+			end
+		end
+	p fib_array
+end
+
+fib(100)
+
