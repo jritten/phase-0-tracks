f9180461382ace08c6c868cce3534276a04f9aa8
Jenna Ritten <jritten@umich.edu>
2017-04-30 21:24:43 -0400

    completed challenge 5.2: arrays and hashes

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -8,35 +8,35 @@
   # else, keep going and add one to  count
 # no items match, return nil
 
-def search_array(arr, num)
-	index = 0
+# def search_array(arr, num)
+# 	index = 0
 
-	arr.each do |number|
-		if number == num
-			p index
-		else
-			index += 1
-		end
-	end
-	p nil
-end
+# 	arr.each do |number|
+# 		if number == num
+# 			p index
+# 		else
+# 			index += 1
+# 		end
+# 	end
+# 	p nil
+# end
 
-search_array([72, 95, 26, 1], 4)
+# search_array([72, 95, 26, 1], 4)
 
-def search_array(arr, num)
-	index = 0
+# def search_array(arr, num)
+# 	index = 0
 
-	arr.length.times do |index|
-		if arr[index] == num
-			puts index
-		else
-			index += 1
-		end
-	end
-	puts nil
-end
+# 	arr.length.times do |index|
+# 		if arr[index] == num
+# 			puts index
+# 		else
+# 			index += 1
+# 		end
+# 	end
+# 	puts nil
+# end
 
-search_array([72, 95, 26, 4], 4)
+# search_array([72, 95, 26, 4], 4)
 
 # Release 1: Calculate Fibonacci Numbers
 
@@ -45,21 +45,36 @@ search_array([72, 95, 26, 4], 4)
 # except for 0 and 1 we started with
 # print results in an array
 
-def fib(bit)
-	fib_array = [0]
-	x = 0
-	y = 1
-	z = 0
-	if bit >= 2
-		fib_array = [*1..bit]
-			until fib_array.count
-				z = x + y
-				y = x
-				z = y 
-				fib_array << j
-			end
-		end
-	p fib_array
+# def fib(bit)
+# 	fib_array = [0]
+# 	x = 0
+# 	y = 1
+# 	z = 0
+# 	if bit >= 2
+# 		fib_array = [*1..bit]
+# 			until fib_array.count
+# 				z = x + y
+# 				y = x
+# 				z = y 
+# 				fib_array << j
+# 			end
+# 		end
+# 	p fib_array
+# end
+
+# fib(100)
+
+def fib(count)
+	fib_array = [*0, 1..number[z]]
+	until fib_array.count > count
+		number[0] = 0
+		number[1] = 1
+		number[z] = number[-1] + number[-2]
+		number[z] = number[-1]
+		fib_array << number[-1]
+		fib(count) == number[-1]
+	end
+p fib(count)
 end
 
 fib(100)
