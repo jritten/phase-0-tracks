a656cd1179dcff9643f79efba570af6d43b9df3b
Jenna Ritten <jritten@umich.edu>
2017-05-01 00:29:16 -0400

    completed 5.3: fibonacci numbers

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -46,7 +46,7 @@ search_array([72, 95, 26, 4], 4)
 # print results in an array
 
 def fib(bit)
-	fib_array = [0]
+	fib_array = [0, 1]
 	x = 0
 	y = 1
 	z = 0
@@ -56,11 +56,44 @@ def fib(bit)
 				z = x + y
 				y = x
 				z = y 
-				fib_array << j
+				fib_array << z
 			end
 		end
 	p fib_array
 end
 
 fib(100)
+
+
+
+def fib_method(number)
+
+	fib_array = [0, 1]
+	index = 0
+
+	while index < number
+		fib_number = fib_array[-1] + fib_array[-2]
+		fib_array << fib_number
+		index += 1
+	end
+
+	fib_100 = 218922995834555169026
+
+	if fib_array[-1] == fib_100
+		puts "Correct!"
+	elsif index == number
+		puts "Fibonacci Sequence: "
+		p fib_array
+		
+	end
+
+end
+
+
+fib_method(100)
+
+
+
+
+
 
