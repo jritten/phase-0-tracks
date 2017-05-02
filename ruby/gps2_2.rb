30697d0b337e64c150aea850f30a538b5a869105
Jenna Ritten <jritten@umich.edu>
2017-05-01 21:53:10 -0400

    gps 2.2: ruby preparation

---
+++ ruby/gps2_2.rb
@@ -0,0 +1,19 @@
+# GPS 2.2: Ruby
+# paired with @
+
+# Release 0: Pseudocode
+
+
+# Release 1: Initial Solution
+
+
+# Release 2: Test Your Solution
+
+
+# Release 3: Refactor
+
+
+# Release 4: Reflect
+
+
+# Release 5: Submit
