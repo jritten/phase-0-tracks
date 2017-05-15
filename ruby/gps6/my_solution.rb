facf95063f4351b793cd2238021e64caeaeb749d
Jenna Ritten <jritten@umich.edu>
2017-05-14 21:54:09 -0400

    completed gps 3.1: ruby

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -57,25 +57,25 @@ class VirusPredictor
 
   private
 
-  def predicted_deaths(population_density, population, state)
+  def predicted_deaths#(population_density, population, state)
     # predicted deaths is solely based on population density
-    if @population_density >= 200
-      number_of_deaths = (@population * 0.4).floor
+    number_of_deaths = if @population_density >= 200
+      (@population * 0.4).floor
     elsif @population_density >= 150
-      number_of_deaths = (@population * 0.3).floor
+      (@population * 0.3).floor
     elsif @population_density >= 100
-      number_of_deaths = (@population * 0.2).floor
+      (@population * 0.2).floor
     elsif @population_density >= 50
-      number_of_deaths = (@population * 0.1).floor
+      (@population * 0.1).floor
     else
-      number_of_deaths = (@population * 0.05).floor
+      (@population * 0.05).floor
     end
 
     print "#{@state} will lose #{number_of_deaths} people in this outbreak"
 
   end
 
-  def speed_of_spread(population_density, state) #in months
+  def speed_of_spread#(population_density, state) #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
     speed = 0.0
@@ -103,7 +103,6 @@ end
 # DRIVER CODE
  # initialize VirusPredictor for each state
 
-
 alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population])
 alabama.virus_effects
 
@@ -116,6 +115,15 @@ california.virus_effects
 alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population])
 alaska.virus_effects
 
+# RELEASE 4: IMPLEMENT A NEW FEATURE
+# Right now this is only running a report for 4 states, and it's not 
+# incredibly DRY. Create a report for all 50 states in a DRY way. 
+# Does this belong inside the class or outside of it? Why?
+
+STATE_DATA.each do |state_name, population_info|
+  new_virus = VirusPredictor.new(state_name, STATE_DATA[state_name][:population_density], STATE_DATA[state_name][:population])
+  new_virus.virus_effects
+end
 
 #=======================================================================
 # Reflection Section
